baa9024c248b959531a6e614df2ce90d1fef9614
abelaraujo <abel2901@gmail.com>
2020-01-01 21:55:02 -0300

    Create ArrayAdapter to students list

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +18,11 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> students = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+        ListView studentList = findViewById(R.id.activity_main_student_list);
+        studentList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students));
+
     }
 }
