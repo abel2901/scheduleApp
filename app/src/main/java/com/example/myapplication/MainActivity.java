256e9f3bb2d8170413d5ccf168467d8a72dc31d8
abelaraujo <abel2901@gmail.com>
2020-02-27 08:15:32 -0300

    Add a new a title and create a new appCompatActivity  On branch master  Changes to be committed: 	modified:   app/src/main/java/com/example/myapplication/MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,18 +7,19 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setTitle("Student List");
         List<String> students = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "Jose"));
         ListView studentList = findViewById(R.id.activity_main_student_list);
