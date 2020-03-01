f5b41d2461244726f939a89336d1157b60fce463
abelaraujo <abel2901@gmail.com>
2020-03-01 12:19:03 -0300

    Code refactor
     Changes to be committed:
    	modified:   app/src/main/java/com/example/myapplication/ui/activity/StudentFormActivity.java

--- app/src/main/java/com/example/myapplication/ui/activity/StudentFormActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activity/StudentFormActivity.java
@@ -13,6 +13,12 @@ import com.example.myapplication.ui.model.Student;
 
 public class StudentFormActivity extends AppCompatActivity {
 
+    private EditText nameInput;
+    private EditText phoneInput;
+    private EditText emailInput;
+
+    final StudentDao dao = new StudentDao();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,26 +26,34 @@ public class StudentFormActivity extends AppCompatActivity {
 
         setTitle("New student");
 
-        final StudentDao dao = new StudentDao();
-
-        final EditText nameField = findViewById(R.id.activity_student_form_name);
-        final EditText phoneField = findViewById(R.id.activity_student_form_phone);
-        final EditText emailField = findViewById(R.id.activity_student_form_email);
+        initializeInputs();
 
         Button saveButton = findViewById(R.id.activity_student_form_save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                String name = nameField.getText().toString();
-                String phone = phoneField.getText().toString();
-                String email = emailField.getText().toString();
-
-                Student createStudent = new Student(name, phone, email);
-                dao.save(createStudent);
-
-                finish();
+                Student createStudent = createStudents();
+                save(createStudent);
             }
         });
+    }
+
+    private void initializeInputs() {
+        nameInput = findViewById(R.id.activity_student_form_name);
+        phoneInput = findViewById(R.id.activity_student_form_phone);
+        emailInput = findViewById(R.id.activity_student_form_email);
+    }
+
+    private void save(Student createStudent) {
+        dao.save(createStudent);
+        finish();
+    }
+
+    private Student createStudents() {
+        String name = nameInput.getText().toString();
+        String phone = phoneInput.getText().toString();
+        String email = emailInput.getText().toString();
+
+        return new Student(name, phone, email);
     }
 }
